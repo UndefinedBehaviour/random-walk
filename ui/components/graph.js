18bbf960b5c45fb30ce9a6b8d9f7eb8b2076ed5b
Agostino Cavarero <agostino.cavarero@gmail.com>
2024-05-06 22:49:59 +0200

    moving graph

--- ui/components/graph.js
+++ ui/components/graph.js
@@ -1,44 +1,66 @@
 class Graph extends HTMLElement {
     static observedAttributes = ["data"];
 
-    data = [0];
+    dataStore = [0];
 
     constructor() {
         super();
     }
 
     async connectedCallback() {
+        const button = document.createElement('button');
+        const pause = () => {
+            button.onclick = restart;
+
+            clearTimeout(this.timeout);
+            button.textContent = 'restart';
+        };
+        const restart = () => {
+            button.onclick = pause;
+
+            setTimeout(func);
+            button.textContent = 'pause';
+        };
+        button.textContent = 'pause';
+        button.onclick = pause;
+        this.append(button);
+        let i = 1;
+        const func = () => {
+            this.dataStore[i] = this.dataStore[i - 1] + (Math.round(Math.random()) * 2 - 1);
+            if (i > this.width) this.dataStore.shift();
+            else {i++};
+            this.renderGraph();
+            this.timeout = setTimeout(func, 1);
+        };
+
+        this.timeout = setTimeout(func, 1);
+    }
+
+    renderGraph() {
         const margin = {top: 10, right: 30, bottom: 30, left: 60};
         const height = 400 - margin.top - margin.bottom;
 
+        d3.select(this).select("svg").remove();
         const svg = d3.select(this).append("svg").attr("width", "98%")
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // const data = await d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
-        //     d => ({ date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value })
-        // );
+        const data = this.dataStore;
 
-        //console.log(this.getBoundingClientRect().width)
-        // const data = Array()
+        this.width = this.getBoundingClientRect().width - margin.left - margin.right;
 
-        const width = this.getBoundingClientRect().width;
-        const data = this.data;
-        for (let i = 1; i < width; i++) {
-            data[i] = data[i - 1] + (Math.round(Math.random()) * 2 - 1);
-        }
+        const x = d3.scaleLinear()
+            .domain([0, this.width])
+            .range([0, this.width]);
 
-        const x = d3.scaleLinear();
-        
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
-        console.log(svg.axisLeft);
         const y = d3.scaleLinear()
-            .domain([d3.min(data, function(d) { return d; }), d3.max(data, function(d) { return d; })])
-            .range([ 0, height ]);
+            .domain([Math.min(d3.min(this.dataStore) * 1.1, -10), Math.max(d3.max(this.dataStore) * 1.1, 10)])
+            .range([height, 0]);
 
         svg.append("g")
             .call(d3.axisLeft(y));
